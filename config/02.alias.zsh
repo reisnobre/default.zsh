34e842483dea75a68e01d286c813bc7f4392a6a3
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2019-11-18 13:11:54 -0300

    update

--- config/02.alias.zsh
+++ config/02.alias.zsh
@@ -8,4 +8,5 @@ alias pip='pip3'
 alias virtualenv='python -m virtualenv'
 alias pa='php artisan'
 alias ctags="`brew --prefix`/bin/ctags"
-alias setup_python="rsync -r ~/code/defaults/python/ . --exclude=.vim; pip install flake8 flake8_docstrings pylint ipython"
+alias setup_python="rsync -r ~/code/defaults/python/ . --exclude=.vim; pip install flake8 flake8_docstrings pylint ipython neovim"
+alias setup_vue="rsync -r ~/code/defaults/vue/ . --exclude=.vim"
